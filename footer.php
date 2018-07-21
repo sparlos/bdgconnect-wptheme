9d748e464d7faa76c5609ebcc326e41df55230b8
Stephen S <mista_fuzzykins@hotmail.com>
2018-07-21 04:05:32 +0000

    add pageid abstraction system w default values

--- footer.php
+++ footer.php
@@ -1,24 +1,26 @@
+<?php
+$fields = CFS()->find_fields( array( 'field_name' => 'footer_page_id' ) );
+$page_id = $fields[0]['options']['default_value'];
+?>
+
 <div class="footer">
   <div class="footer__item">
     <span class="footer__text">
-      <a href=""><?php echo CFS()->get('footer_address', 85); ?></a>
+      <a href=""><?php echo CFS()->get('footer_address', $page_id); ?></a>
     </span>
   </div>
   <div class="footer__item">
     <span>
-
-      <?php $icon_one = CFS()->get('first_icon', 85);
-      
+      <?php $icon_one = CFS()->get('first_icon', $page_id);
       ?>
-
-      <a target="_blank" href="<?php echo CFS()->get('instagram', 85); ?>"><i class="fab fa-<?php echo reset($icon_one); ?>"></i></a>
-      <a target="_blank" href="<?php echo CFS()->get('twitter', 85); ?>"><i class="fab fa-twitter"></i></a>
-      <a target="_blank" href="<?php echo CFS()->get('youtube', 85); ?>"><i class="fab fa-youtube"></i></a>
-      <a href="mailto:<?php echo CFS()->get('email', 85); ?>"><i class="far fa-envelope"></i></a>
+      <a target="_blank" href="<?php echo CFS()->get('instagram', $page_id); ?>"><i class="fab fa-<?php echo reset($icon_one); ?>"></i></a>
+      <a target="_blank" href="<?php echo CFS()->get('twitter', $page_id); ?>"><i class="fab fa-twitter"></i></a>
+      <a target="_blank" href="<?php echo CFS()->get('youtube', $page_id); ?>"><i class="fab fa-youtube"></i></a>
+      <a href="mailto:<?php echo CFS()->get('email', $page_id); ?>"><i class="far fa-envelope"></i></a>
     </span>
   </div>
   <div class="footer__item">
-    <span class="footer__text"><a href="mailto:<?php echo CFS()->get('email', 85); ?>"><?php echo CFS()->get('email', 85); ?></a></span>
+    <span class="footer__text"><a href="mailto:<?php echo CFS()->get('email', $page_id); ?>"><?php echo CFS()->get('email', $page_id); ?></a></span>
   </div>
 </div>
 
