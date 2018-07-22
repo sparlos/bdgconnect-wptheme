b5cb6eacd409ac98542035f3ef805df137786355
Stephen S <mista_fuzzykins@hotmail.com>
2018-07-22 16:32:38 +0000

    integrate php to footer

--- footer.php
+++ footer.php
@@ -11,12 +11,34 @@ $page_id = $page->ID;
   </div>
   <div class="footer__item">
     <span>
-      <?php $icon_one = CFS()->get('first_icon', $page_id);
+      <?php $first_icon = CFS()->get('first_icon', $page_id);
+      reset($first_icon);
+      $second_icon = CFS()->get('second_icon', $page_id);
+      reset($second_icon);
+      $third_icon = CFS()->get('third_icon', $page_id);
+      reset($third_icon);
+      $fourth_icon = CFS()->get('fourth_icon', $page_id);
+      reset($fourth_icon);
+      
+      $first_icon_link = CFS()->get('first_icon_link', $page_id);
+      $second_icon_link = CFS()->get('second_icon_link', $page_id);
+      $third_icon_link = CFS()->get('third_icon_link', $page_id);
+      $fourth_icon_link = CFS()->get('fourth_icon_link', $page_id);
+      
+      $icons = array($first_icon, $second_icon, $third_icon, $fourth_icon);
+      $icon_links = array($first_icon_link, $second_icon_link, $third_icon_link, $fourth_icon_link);
+      
+      foreach($icons as $key=>$icon) {
+        if(reset($icon) == 'Email') {
+          $icon_links[$key] = 'mailto:' . $icon_links[$key];
+        }
+      }
+      
       ?>
-      <a target="_blank" href="<?php echo CFS()->get('instagram', $page_id); ?>"><i class="fab fa-<?php echo reset($icon_one); ?>"></i></a>
-      <a target="_blank" href="<?php echo CFS()->get('twitter', $page_id); ?>"><i class="fab fa-twitter"></i></a>
-      <a target="_blank" href="<?php echo CFS()->get('youtube', $page_id); ?>"><i class="fab fa-youtube"></i></a>
-      <a href="mailto:<?php echo CFS()->get('email', $page_id); ?>"><i class="far fa-envelope"></i></a>
+      <a target="_blank" href="<?php echo $icon_links[0]; ?>"><i class="<?php echo key($first_icon); ?>"></i></a>
+      <a target="_blank" href="<?php echo $icon_links[1]; ?>"><i class="<?php echo key($second_icon); ?>"></i></a>
+      <a target="_blank" href="<?php echo $icon_links[2]; ?>"><i class="<?php echo key($third_icon); ?>"></i></a>
+      <a target="_blank" href="<?php echo $icon_links[3]; ?>"><i class="<?php echo key($fourth_icon); ?>"></i></a>
     </span>
   </div>
   <div class="footer__item">
